3b1726e906d0b32830f5b40f520e94306268ce0e
dave-carroll7 <llorrac.evad@gmail.com>
2025-06-27 14:08:14 -0400

    refactor: unit tests + add comments

--- test/actions/SweepCCTPDepositAccount.t.sol
+++ test/actions/SweepCCTPDepositAccount.t.sol
@@ -6,23 +6,16 @@ import {Vm, VmSafe} from "forge-std/src/Vm.sol";
 
 import {IERC20} from "@openzeppelin-contracts/token/ERC20/IERC20.sol";
 
-import {ERC20Mock} from "../mocks/ERC20Mock.sol";
-
 import {InstructionLib} from "../../src/libraries/Instruction.sol";
 import {InstructionForkTestContext} from "../utils/InstructionForkTestContext.sol";
 
 import {IOtimDelegate} from "../../src/IOtimDelegate.sol";
 
-import {FeeTokenRegistry} from "../../src/infrastructure/FeeTokenRegistry.sol";
-import {Treasury} from "../../src/infrastructure/Treasury.sol";
-
 import {SkipGoFeeOracle} from "../../src/actions/oracles/SkipGoFeeOracle.sol";
-
+import {ICCTPRelayer} from "@skip-go-evm-contracts/CCTPRelayer/src/interfaces/ICCTPRelayer.sol";
 import {ITokenController} from "../../src/actions/external/ITokenController.sol";
 
 import {IOtimFee} from "../../src/actions/fee-models/interfaces/IOtimFee.sol";
-
-import {ICCTPRelayer} from "@skip-go-evm-contracts/CCTPRelayer/src/interfaces/ICCTPRelayer.sol";
 
 import {ISweepCCTPDepositAccountAction} from "../../src/actions/interfaces/ISweepCCTPDepositAccountAction.sol";
 import {SweepCCTPDepositAccountAction} from "../../src/actions/SweepCCTPDepositAccountAction.sol";
@@ -40,18 +33,17 @@ contract SweepCCTPDepositAccountTest is InstructionForkTestContext {
 
     address public constant SEPOLIA_USDC_WHALE = address(0x1fD9611f009fcB8Bec0A4854FDcA0832DfdB04E3);
 
-    uint256 public constant DEFAULT_SKIP_GO_FEE = 2000;
-
-    VmSafe.Wallet public depositor;
-
     SkipGoFeeOracle public skipGoFeeOracle;
-
     SweepCCTPDepositAccountAction public sweepCCTPDepositAccountAction;
 
-    address public DEFAULT_DEPOSIT_ACCOUNT;
+    VmSafe.Wallet public depositor = vm.createWallet("depositor");
 
-    address public DEFAULT_DEPOSITOR;
-    uint256 public DEFAULT_THRESHOLD;
+    uint256 public constant DEFAULT_SKIP_GO_FEE = 2000;
+
+    address public DEFAULT_DEPOSITOR = depositor.addr;
+    uint256 public DEFAULT_THRESHOLD = DEFAULT_SKIP_GO_FEE * 5;
+
+    address public DEFAULT_DEPOSIT_ACCOUNT;
 
     IOtimFee.Fee public DEFAULT_FEE;
 
@@ -73,8 +65,6 @@ contract SweepCCTPDepositAccountTest is InstructionForkTestContext {
 
         vm.createSelectFork(rpcUrl);
 
-        depositor = vm.createWallet("depositor");
-
         vm.startPrank(SEPOLIA_USDC_WHALE);
         IERC20(SEPOLIA_USDC).transfer(address(user), IERC20(SEPOLIA_USDC).balanceOf(SEPOLIA_USDC_WHALE));
         vm.stopPrank();
@@ -95,9 +85,6 @@ contract SweepCCTPDepositAccountTest is InstructionForkTestContext {
 
         actionManager.addAction(address(sweepCCTPDepositAccountAction));
 
-        DEFAULT_DEPOSITOR = depositor.addr;
-        DEFAULT_THRESHOLD = DEFAULT_SKIP_GO_FEE * 5;
-
         DEFAULT_ACTION_ARGS = ISweepCCTPDepositAccountAction.SweepCCTPDepositAccount({
             depositor: DEFAULT_DEPOSITOR,
             destinationDomain: 4,
@@ -117,6 +104,7 @@ contract SweepCCTPDepositAccountTest is InstructionForkTestContext {
         DEFAULT_ARGS = abi.encode(DEFAULT_ACTION_ARGS);
     }
 
+    /// @notice test that sweeping USDC to the CCTP relayer works as expected
     function test_sweepCCTPDepositAccount_happyPath() public {
         vm.pauseGasMetering();
 
@@ -130,6 +118,7 @@ contract SweepCCTPDepositAccountTest is InstructionForkTestContext {
 
         assertEq(IERC20(SEPOLIA_USDC).balanceOf(DEFAULT_DEPOSIT_ACCOUNT), DEFAULT_THRESHOLD + 1);
 
+        // check that the CCTP transfer was initiated correctly
         // don't check the nonce
         vm.expectEmit(false, true, true, true);
         emit DepositForBurn(
@@ -143,6 +132,7 @@ contract SweepCCTPDepositAccountTest is InstructionForkTestContext {
             bytes32(uint256(0))
         );
 
+        // check that the SkipGo fee was paid correctly
         // don't check the nonce
         vm.expectEmit(false, true, false, false);
         emit ICCTPRelayer.PaymentForRelay(0, feeAmount);
@@ -154,9 +144,11 @@ contract SweepCCTPDepositAccountTest is InstructionForkTestContext {
         user.executeInstruction(instruction, instructionSig);
         vm.pauseGasMetering();
 
+        // check that the deposit account is empty after the sweep
         assertEq(IERC20(SEPOLIA_USDC).balanceOf(DEFAULT_DEPOSIT_ACCOUNT), 0);
     }
 
+    /// @notice test that sweeping USDC to the CCTP relayer works as expected even when the deposit account has more USDC than the burn limit
     function test_sweepCCTPDepositAccount_overBurnLimit() public {
         vm.pauseGasMetering();
 
@@ -172,6 +164,7 @@ contract SweepCCTPDepositAccountTest is InstructionForkTestContext {
 
         assertEq(IERC20(SEPOLIA_USDC).balanceOf(DEFAULT_DEPOSIT_ACCOUNT), maxBurnPerMessage * 5);
 
+        // check that the CCTP transfer was initiated correctly
         // don't check the nonce
         vm.expectEmit(false, true, true, true);
         emit DepositForBurn(
@@ -185,6 +178,7 @@ contract SweepCCTPDepositAccountTest is InstructionForkTestContext {
             bytes32(uint256(0))
         );
 
+        // check that the SkipGo fee was paid correctly
         // don't check the nonce
         vm.expectEmit(false, true, false, false);
         emit ICCTPRelayer.PaymentForRelay(0, feeAmount);
@@ -196,9 +190,11 @@ contract SweepCCTPDepositAccountTest is InstructionForkTestContext {
         user.executeInstruction(instruction, instructionSig);
         vm.pauseGasMetering();
 
+        // check that the deposit account was swept up to the max burn limit (minus the fee)
         assertEq(IERC20(SEPOLIA_USDC).balanceOf(DEFAULT_DEPOSIT_ACCOUNT), maxBurnPerMessage * 4 - feeAmount);
     }
 
+    /// @notice test that sweeping USDC to the CCTP relayer works as expected when the deposit account also has ETH balance
     function test_sweepCCTPDepositAccount_withEthBalance() public {
         vm.pauseGasMetering();
 
@@ -210,6 +206,7 @@ contract SweepCCTPDepositAccountTest is InstructionForkTestContext {
 
         assertEq(IERC20(SEPOLIA_USDC).balanceOf(DEFAULT_DEPOSIT_ACCOUNT), DEFAULT_THRESHOLD + 1);
 
+        // deal some ETH to the deposit account
         vm.deal(DEFAULT_DEPOSIT_ACCOUNT, 100);
         assertEq(DEFAULT_DEPOSIT_ACCOUNT.balance, 100);
 
@@ -220,13 +217,56 @@ contract SweepCCTPDepositAccountTest is InstructionForkTestContext {
         user.executeInstruction(instruction, instructionSig);
         vm.pauseGasMetering();
 
+        // check that the deposit account has no ETH or USDC balance after the sweep
         assertEq(DEFAULT_DEPOSIT_ACCOUNT.balance, 0);
+        assertEq(IERC20(SEPOLIA_USDC).balanceOf(DEFAULT_DEPOSIT_ACCOUNT), 0);
+
+        // check that the ETH balance was credited to the depositor
+        assertEq(DEFAULT_DEPOSITOR.balance, 100);
+    }
+
+    /// @notice test that the Instruction succeeds even if the deposit account is perminently deployed and can't be self-destructed
+    /// @dev this case will never happen if the Action is used as intended, but it is technically possible for the owner to permanently deploy the deposit account
+    function test_sweepCCTPDepositAccount_alreadyDeployed() public {
+        vm.pauseGasMetering();
+
+        buildInstruction();
+
+        // permanently deploy the deposit account
+        vm.prank(address(user));
+        CCTPDepositAccount cctpDepositAccount = new CCTPDepositAccount(
+            SEPOLIA_USDC,
+            SEPOLIA_CCTP_RELAYER,
+            SEPOLIA_TOKEN_MINTER,
+            address(skipGoFeeOracle),
+            DEFAULT_ACTION_ARGS.depositor,
+            DEFAULT_ACTION_ARGS.destinationDomain,
+            DEFAULT_ACTION_ARGS.destinationMintRecipient
+        );
+        vm.etch(DEFAULT_DEPOSIT_ACCOUNT, address(cctpDepositAccount).code);
+
+        vm.startPrank(address(user));
+        IERC20(SEPOLIA_USDC).transfer(DEFAULT_DEPOSIT_ACCOUNT, DEFAULT_THRESHOLD + 1);
+        vm.stopPrank();
+
+        assertEq(IERC20(SEPOLIA_USDC).balanceOf(DEFAULT_DEPOSIT_ACCOUNT), DEFAULT_THRESHOLD + 1);
+
+        vm.expectEmit();
+        emit IOtimDelegate.InstructionExecuted(instructionId, 1);
+
+        vm.resumeGasMetering();
+        user.executeInstruction(instruction, instructionSig);
+        vm.pauseGasMetering();
+
+        // check that the deposit account was swept
         assertEq(IERC20(SEPOLIA_USDC).balanceOf(DEFAULT_DEPOSIT_ACCOUNT), 0);
     }
 
+    /// @notice test that the Instruction reverts when the depositor is set to the zero address
     function test_sweepCCTPDepositAccount_depositorZero() public {
         vm.pauseGasMetering();
 
+        // set the depositor to the zero address
         DEFAULT_ACTION_ARGS.depositor = address(0);
 
         buildInstruction(DEFAULT_SALT, DEFAULT_MAX_EXECUTIONS, DEFAULT_ACTION, abi.encode(DEFAULT_ACTION_ARGS));
@@ -239,9 +279,11 @@ contract SweepCCTPDepositAccountTest is InstructionForkTestContext {
         vm.pauseGasMetering();
     }
 
+    /// @notice test that the Instruction reverts when the destinationMintRecipient is set to the zero address
     function test_sweepCCTPDepositAccount_destinationMintRecipientZero() public {
         vm.pauseGasMetering();
 
+        // set the destinationMintRecipient to the zero address
         DEFAULT_ACTION_ARGS.destinationMintRecipient = bytes32(0);
 
         buildInstruction(DEFAULT_SALT, DEFAULT_MAX_EXECUTIONS, DEFAULT_ACTION, abi.encode(DEFAULT_ACTION_ARGS));
@@ -254,11 +296,13 @@ contract SweepCCTPDepositAccountTest is InstructionForkTestContext {
         vm.pauseGasMetering();
     }
 
+    /// @notice test that the Instruction reverts when the deposit account balance is below the threshold
     function test_sweepCCTPDepositAccount_balanceUnderThreshold() public {
         vm.pauseGasMetering();
 
         buildInstruction();
 
+        // set the deposit account balance to below the threshold
         vm.startPrank(address(user));
         IERC20(SEPOLIA_USDC).transfer(DEFAULT_DEPOSIT_ACCOUNT, DEFAULT_THRESHOLD - 1);
         vm.stopPrank();
@@ -271,51 +315,24 @@ contract SweepCCTPDepositAccountTest is InstructionForkTestContext {
         vm.pauseGasMetering();
     }
 
+    /// @notice test that the Instruction reverts when the deposit account balance is below the SkipGo fee amount
     function test_sweepCCTPDepositAccount_insufficientSkipGoFeeBalance() public {
         vm.pauseGasMetering();
 
+        // set threshold to zero to avoid the balance under threshold check
         DEFAULT_ACTION_ARGS.threshold = 0;
 
         buildInstruction(DEFAULT_SALT, DEFAULT_MAX_EXECUTIONS, DEFAULT_ACTION, abi.encode(DEFAULT_ACTION_ARGS));
 
         uint256 feeAmount = skipGoFeeOracle.getFee(DEFAULT_ACTION_ARGS.destinationDomain);
 
+        // set the deposit account balance to below the SkipGo fee amount
         vm.startPrank(address(user));
         IERC20(SEPOLIA_USDC).transfer(DEFAULT_DEPOSIT_ACCOUNT, feeAmount - 1);
         vm.stopPrank();
 
         bytes memory result = abi.encodeWithSelector(CCTPDepositAccount.InsufficientBalanceForSkipGoFee.selector);
         vm.expectRevert(abi.encodeWithSelector(IOtimDelegate.ActionExecutionFailed.selector, instructionId, result));
-
-        vm.resumeGasMetering();
-        user.executeInstruction(instruction, instructionSig);
-        vm.pauseGasMetering();
-    }
-
-    function test_sweepCCTPDepositAccount_alreadyDeployed() public {
-        vm.pauseGasMetering();
-
-        buildInstruction();
-
-        vm.prank(address(user));
-        CCTPDepositAccount cctpDepositAccount = new CCTPDepositAccount(
-            SEPOLIA_USDC,
-            SEPOLIA_CCTP_RELAYER,
-            SEPOLIA_TOKEN_MINTER,
-            address(skipGoFeeOracle),
-            DEFAULT_ACTION_ARGS.depositor,
-            DEFAULT_ACTION_ARGS.destinationDomain,
-            DEFAULT_ACTION_ARGS.destinationMintRecipient
-        );
-
-        vm.etch(DEFAULT_DEPOSIT_ACCOUNT, address(cctpDepositAccount).code);
-
-        vm.startPrank(address(user));
-        IERC20(SEPOLIA_USDC).transfer(DEFAULT_DEPOSIT_ACCOUNT, DEFAULT_THRESHOLD + 1);
-        vm.stopPrank();
-
-        vm.expectEmit();
-        emit IOtimDelegate.InstructionExecuted(instructionId, 1);
 
         vm.resumeGasMetering();
         user.executeInstruction(instruction, instructionSig);
