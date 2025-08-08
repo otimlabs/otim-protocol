db331dbc9c97ec62f46a6c2dac459b494f7eefbd
Jrachman <10068250+Jrachman@users.noreply.github.com>
2025-08-07 20:16:16 -0700

    chore: added transfer ownership scripts for treasury and fee token registry

--- script/governance/access-control/GrantActionManagerOwnerRole.s.sol
+++ script/governance/access-control/GrantActionManagerOwnerRole.s.sol
@@ -5,26 +5,26 @@ import {Script} from "forge-std/src/Script.sol";
 
 import {IAccessControl} from "@openzeppelin-contracts/access/IAccessControl.sol";
 
-/// @title GrantOwnerRole
+/// @title GrantActionManagerOwnerRole
 /// @author Otim Labs, Inc.
 /// @notice script to grant the owner role to a new address on ActionManager
-contract GrantOwnerRole is Script {
+contract GrantActionManagerOwnerRole is Script {
     /// @dev make sure to run `cp .env_example .env` and fill in each variable
     /// then run `source .env` in your terminal before copying and pasting one of the commands below
 
     // commands to grant the owner role to a new address on ActionManager (enter new owner address interactively):
     //
-    // - with private key (on Anvil): forge script GrantOwnerRole --broadcast --fork-url http://localhost:8545 --private-key $ANVIL_OWNER_PK
-    // - with private key:            forge script GrantOwnerRole --broadcast --rpc-url $RPC_URL --private-key $OWNER_PK
-    // - with Ledger:                 forge script GrantOwnerRole --broadcast --rpc-url $RPC_URL --ledger
-    // - with AWS:                    forge script GrantOwnerRole --broadcast --rpc-url $RPC_URL --aws
+    // - with private key (on Anvil): forge script GrantActionManagerOwnerRole --broadcast --fork-url http://localhost:8545 --private-key $ANVIL_OWNER_PK
+    // - with private key:            forge script GrantActionManagerOwnerRole --broadcast --rpc-url $RPC_URL --private-key $OWNER_PK
+    // - with Ledger:                 forge script GrantActionManagerOwnerRole --broadcast --rpc-url $RPC_URL --ledger
+    // - with AWS:                    forge script GrantActionManagerOwnerRole --broadcast --rpc-url $RPC_URL --aws
 
     // commands to grant the owner role to a new address on ActionManager (enter new owner address as a command line argument):
     //
-    // - with private key (on Anvil): forge script GrantOwnerRole --sig "run(address)" --broadcast --fork-url http://localhost:8545 --private-key $ANVIL_OWNER_PK <newOwner>
-    // - with private key:            forge script GrantOwnerRole --sig "run(address)" --broadcast --rpc-url $RPC_URL --private-key $OWNER_PK <newOwner>
-    // - with Ledger:                 forge script GrantOwnerRole --sig "run(address)" --broadcast --rpc-url $RPC_URL --ledger <newOwner>
-    // - with AWS:                    forge script GrantOwnerRole --sig "run(address)" --broadcast --rpc-url $RPC_URL --aws <newOwner>
+    // - with private key (on Anvil): forge script GrantActionManagerOwnerRole --sig "run(address)" --broadcast --fork-url http://localhost:8545 --private-key $ANVIL_OWNER_PK <newOwner>
+    // - with private key:            forge script GrantActionManagerOwnerRole --sig "run(address)" --broadcast --rpc-url $RPC_URL --private-key $OWNER_PK <newOwner>
+    // - with Ledger:                 forge script GrantActionManagerOwnerRole --sig "run(address)" --broadcast --rpc-url $RPC_URL --ledger <newOwner>
+    // - with AWS:                    forge script GrantActionManagerOwnerRole --sig "run(address)" --broadcast --rpc-url $RPC_URL --aws <newOwner>
 
     /// @dev this is the same as the DEFAULT_ADMIN_ROLE in OpenZeppelin's AccessControl.sol
     bytes32 public constant OWNER_ROLE = 0x00;
@@ -32,7 +32,7 @@ contract GrantOwnerRole is Script {
     error OwnerRoleAlreadyGranted();
 
     function run() public {
-        address newOwner = vm.promptAddress("Enter new owner address");
+        address newOwner = vm.promptAddress("Enter new ActionManager owner address");
 
         run(newOwner);
     }
